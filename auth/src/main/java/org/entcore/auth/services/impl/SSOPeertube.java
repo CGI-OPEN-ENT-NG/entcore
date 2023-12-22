d9c5e06ee89dca1346ce457ea11de8ebb2c17cba
alex.delaveau <alex.delaveau@cgi.com>
2023-12-22 15:40:59 +0100

    feat(connector): #STAT-157 add access stat for Peertube

--- auth/src/main/java/org/entcore/auth/services/impl/SSOPeertube.java
+++ auth/src/main/java/org/entcore/auth/services/impl/SSOPeertube.java
@@ -5,11 +5,18 @@ import io.vertx.core.Handler;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
+import org.entcore.common.events.EventHelper;
+import org.entcore.common.events.EventStore;
+import org.entcore.common.events.EventStoreFactory;
 import org.entcore.common.neo4j.Neo4j;
 import org.entcore.common.neo4j.Neo4jResult;
+import org.entcore.common.user.UserInfos;
+import org.entcore.common.user.UserUtils;
 import org.opensaml.saml2.core.Assertion;
 
 public class SSOPeertube extends AbstractSSOProvider {
+
+    private final EventStore eventStore = EventStoreFactory.getFactory().getEventStore(this.getClass().getSimpleName());
     @Override
     public void generate(EventBus eb, String userId, String host, String serviceProviderEntityId, Handler<Either<String, JsonArray>> handler) {
         String query = "MATCH (u:User {id:{userId}}) RETURN u.id as id, u.login as login, u.displayName as displayName, u.email as email, u.externalId as externalId";
@@ -26,6 +33,11 @@ public class SSOPeertube extends AbstractSSOProvider {
             result.add(new JsonObject().put("displayName", user.getString("displayName", "")));
             result.add(new JsonObject().put("email", user.getString("email", "")));
             result.add(new JsonObject().put("externalId", user.getString("externalId", "")));
+            UserUtils.getUserInfos(eb, userId, userInfos -> {
+                if (userInfos != null) {
+                    createStatsEvent(userInfos, host);
+                }
+            });
             handler.handle(new Either.Right<>(result));
         }));
     }
@@ -33,5 +45,10 @@ public class SSOPeertube extends AbstractSSOProvider {
     @Override
     public void execute(Assertion assertion, Handler<Either<String, Object>> handler) {
         handler.handle(new Either.Left<String, Object>("execute function ot available on SSO Peertube Implementation"));
+    }
+
+    private void createStatsEvent(UserInfos user, String host) {
+        JsonObject event = new JsonObject().put("service", host).put("connector-type", "saml").put("saml-type", this.getClass().getSimpleName());
+        eventStore.createAndStoreEvent(EventHelper.ACCESS_EVENT, user, event);
     }
 }
